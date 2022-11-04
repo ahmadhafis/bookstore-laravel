4cbf021b768564e1ef5d3817cadd2cd004c0b48e
ahmadhafis <ahmadhafis296@gmail.com>
2022-11-04 15:41:22 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'login');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/landingpage', [LandingpageController::class], 'index')->name('landingpage');
+
